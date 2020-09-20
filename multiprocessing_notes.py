1782c231ced03abc62981862e71998c3f1de2221
lalebdi <loai.alebdi@gmail.com>
2020-09-20 17:03:16 -0400

    done with the array. working on queue next

--- multiprocessing_notes.py
+++ multiprocessing_notes.py
@@ -1,4 +1,5 @@
 from multiprocessing import Process, Value, Array, Lock
+from multiprocessing import Queue # <-- a Queue follow a linear dat principle that processes first come first served 
 import time
 import os
 
@@ -6,25 +7,27 @@ import os
 # 1- Value
 # 2- Array
 
-def add_100(number, lock):
+# a Queue can be used for safe data exchanges
+
+def add_100(numbers, lock):
     for i in range(100):
         time.sleep(0.01)
-        with lock:
-            number.value += 1
-# or
-        # lock.acquire()
-        # number.value += 1
-        # lock.release()
+        # running a number for loop will give you the same array because you are creating a local variable
+        for i in range(len(numbers)):
+            with lock:
+                numbers[i]+= 1
+
+
 
 if __name__ == "__main__":
 
     lock = Lock()
-    shared_number = Value('i', 0) # takes 2 arguments. 1- is the data type as a string (here its an integer as a string). 2- a starting value.
-    print("Number at beginning is", shared_number.value)
+    shared_array = Array('d', [0.0, 100.0, 200.0]) # takes 2 arguments. 1- is the data type as a string (here its a double as a string). 2- a starting list.
+    print("Array at beginning is", shared_array[:])
 
     # now creating 2 processes that should modify the shared number
-    p1 = Process(target= add_100, args = (shared_number,lock))
-    p2 = Process(target= add_100, args = (shared_number, lock))
+    p1 = Process(target= add_100, args = (shared_array,lock))
+    p2 = Process(target= add_100, args = (shared_array, lock))
 
 # we start the processes
     p1.start()
@@ -34,4 +37,50 @@ if __name__ == "__main__":
     p1.join()
     p2.join()
 
-    print('number at the end is ', shared_number.value) # you will get a different number everytime you runs this because the race condition happenened. to prevent it use a lock
+    print("Array at beginning is", shared_array[:]) # you will get a different number everytime you runs this because the race condition happenened. to prevent it use a lock
+
+
+
+
+
+
+
+
+#     # with a share value:
+#     from multiprocessing import Process, Value, Array, Lock
+# import time
+# import os
+
+# # to share data between processes. Since they don't share a memory space, the dont have access to the same data. they need spcial memory share object to share data. there are 2 memory share objects that we can use:
+# # 1- Value
+# # 2- Array
+
+# def add_100(number, lock):
+#     for i in range(100):
+#         time.sleep(0.01)
+#         with lock:
+#             number.value += 1
+# # or
+#         # lock.acquire()
+#         # number.value += 1
+#         # lock.release()
+
+# if __name__ == "__main__":
+
+#     lock = Lock()
+#     shared_number = Value('i', 0) # takes 2 arguments. 1- is the data type as a string (here its an integer as a string). 2- a starting value.
+#     print("Number at beginning is", shared_number.value)
+
+#     # now creating 2 processes that should modify the shared number
+#     p1 = Process(target= add_100, args = (shared_number,lock))
+#     p2 = Process(target= add_100, args = (shared_number, lock))
+
+# # we start the processes
+#     p1.start()
+#     p2.start()
+
+#     # wait for them to complete
+#     p1.join()
+#     p2.join()
+
+#     print('number at the end is ', shared_number.value) # you will get a different number everytime you runs this because the race condition happenened. to prevent it use a lock
