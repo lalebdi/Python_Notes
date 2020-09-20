d1dbfee9c20ed5f241e391b930e85bf78dfa7cd1
lalebdi <loai.alebdi@gmail.com>
2020-09-20 16:34:14 -0400

    template ready

---
+++ multiprocessing_notes.py
@@ -0,0 +1,28 @@
+from multiprocessing import Process
+import os
+
+
+def square_numbers():
+    for i in range(1000):
+        i*i
+
+
+
+if __name__ == "__main__":
+    processes = []
+    num_processes = os.cpu_count()
+
+    # create processes and asign a function for each process
+    for i in range(num_processes):
+        process = Process(target= square_numbers)
+        processes.append(process)
+
+
+    # starting all processes
+    for process in processes:
+        process.star()
+
+    # wait for all processes finish
+    # block the main program until these processes are finished
+    for process in processes:
+        process.join()
