e873eefd87f1ae944e55b5a6e43cbc58b1f20525
lalebdi <loai.alebdi@gmail.com>
2020-09-27 16:26:32 -0400

    done with definitions

---
+++ OOP_principles.py
@@ -0,0 +1,5 @@
+# OOP pillers are:
+# Encapsulation. -> you can hide the inner details of a class or certain data and share what is needed. So, instead of people accessing some data directly, they access it through setters and getters. In Python, this is done using "Property". 
+# Inheritence. -> allows us to have certain attributes for objects because they defined in a base class. 
+# polymorphisim -> we can treat objects as the same thing. 
+
