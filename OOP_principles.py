1a57585da131dc33034ac0bf1b4f12c841b08b39
lalebdi <loai.alebdi@gmail.com>
2020-09-27 16:34:13 -0400

    done with the basic layout

--- OOP_principles.py
+++ OOP_principles.py
@@ -3,3 +3,35 @@
 # Inheritence. -> allows us to have certain attributes for objects because they defined in a base class. 
 # polymorphisim -> we can treat objects as the same thing. 
 
+
+class Customer:
+    def __init__(self, name, membership_type):
+        self.name = name
+        self.membership_type = membership_type
+
+    def update_membership(self, new_membership):
+        print('Calcualting Costs')
+        self.membership_type = new_membership
+
+    def __str__(self):
+        return self.name + " " + self.membership_type
+
+    def print_all_customers(customers):
+        for customer in customers:
+            print(customer)
+
+    def __eq__(self, other):
+        if self.name == other.name and self.membership_type == other.membership_type:
+            return True
+
+        return False
+
+    __hash__ = None
+
+    __repr__ = __str__
+
+
+customers = [Customer('Bella', 'Gold') , Customer('Snowball', 'Silver'), Customer('Maruysa', "Bronze")]
+
+print(customers)
+
