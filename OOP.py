9bd25beac7cbd5f1f7444bc7b3c734d5e4e8b1aa
lalebdi <loai.alebdi@gmail.com>
2020-09-27 16:15:28 -0400

    done with the overriding methods

--- OOP.py
+++ OOP.py
@@ -9,9 +9,24 @@ class Customer:
     def update_membership(self, new_membership): #anytime you want to a method to appear on an instatiated object you need to add "self" as an attribute. not using "self" renders the method un-envokeable -> you will be a TypeError stating that "0 positional arguments but 1 was given". 
         self.membership_type = new_membership
 
-    def __str__(self): #this is going to be envoked anytime we try to convert an object into a string
+    def __str__(self): #this is going to be envoked anytime we try to convert an object into a string. Without this method we would get a memory refernce when we print a customer.
         print("converting into a string")
         return self.name + " " + self.membership_type
+    
+    def print_all_customers(customers):
+        for customer in customers:
+            print(customer)
+
+    def __eq__(self, other): # "other" refers to what we are comparing the customers to. if we did not over ride the equals method, we would get comparsions by memory location.
+        if self.name == other.name and self.membership_type == other.membership_type:
+            return True
+        
+        return False
+        
+    __hash__= None # this isn't going to break anything. the only thing of this is not going to be able to use Customers in a hashable datastructures
+
+    __repr__ = __str__ # this is for representation. it will override 
+
 
 # stattic methods (the methods inside a class that don't have "self" as a parameter): they are methods that are not attached to any object created but instead are envoked on the class itself. 
 
@@ -39,4 +54,10 @@ print(customers_b[1].membership_type) # this is the updated membership type that
 print(customers_b[0])
 # after envoking the __str__method:
 print(customers_b[0])
+
+
+Customer.print_all_customers(customers_b)
+print(customers_b[0] == customers_b[1])
+
+print(customers_b)
 
