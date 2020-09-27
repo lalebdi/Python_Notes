0ff41e48e1b44710b6d6b140ac1fe391591cc85f
lalebdi <loai.alebdi@gmail.com>
2020-09-26 20:26:32 -0400

    second commit

---
+++ OOP.py
@@ -0,0 +1,2 @@
+# classes represent something. you can instansiate classes into objects. So we define a structure in a class and use that class to create objects.
+# objects are examples of classes.
